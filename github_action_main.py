ae5c96127606b4ce0b1bf519aaa04380869c3574
Danny Mandel <danny.mandel@gmail.com>
2023-04-11 18:01:21 -0700

    Fix syntax errors

--- github_action_main.py
+++ github_action_main.py
@@ -2,10 +2,10 @@ import os
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    # my_input = os.environ["INPUT_MYINPUT"]
     print(f"environment variables are {os.environ}")
-    my_output = f"Hello {my_input}"
-    print(f"::set-output name=myOutput::{my_output}")
+    # my_output = f"Hello {my_input}"
+    print(f"::set-output name=myOutput::foo")
 
 
 if __name__ == "__main__":
