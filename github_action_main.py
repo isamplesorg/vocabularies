dffa74c035f17e086b5b8e9daf17cffe1d3a4018
Danny Mandel <danny.mandel@gmail.com>
2023-04-14 09:57:02 -0700

    Properly get the args to the python subprocess

--- github_action_main.py
+++ github_action_main.py
@@ -17,13 +17,16 @@ def main():
         if path is None:
             print("Did not receive a valid path argument so we cannot run.")
             sys.exit(-1)
-        subprocess.run("make cache")
+        subprocess.run(["/usr/bin/make",  "cache"])
         material_json = os.path.join(path, "material_hierarchy.json")
-        subprocess.run(f"python tools/vocab.py -s cache/vocabularies.db uijson mat:materialsvocabulary -e >& {material_json}")
-        material_json = os.path.join(path, "sampledFeature_hierarchy.json")
-        subprocess.run(f"python tools/vocab.py -s cache/vocabularies.db uijson sf:sampledfeaturevocabulary -e >& {material_json}")
-        material_json = os.path.join(path, "specimenType_hierarchy.json")
-        subprocess.run(f"python tools/vocab.py -s cache/vocabularies.db uijson spec:specimentypevocabulary -e >& {material_json}")
+        with open(material_json, "w") as f:
+            subprocess.run([sys.executable, "tools/vocab.py", "-s", "cache/vocabularies.db", "uijson", "mat:materialsvocabulary", "-e"], stdout=f)
+        sampled_feature_json = os.path.join(path, "sampledFeature_hierarchy.json")
+        with open(sampled_feature_json, "w") as f:
+            subprocess.run([sys.executable, "tools/vocab.py", "-s", "cache/vocabularies.db", "uijson", "sf:sampledfeaturevocabulary", "-e"], stdout=f)
+        specimentype_json = os.path.join(path, "specimenType_hierarchy.json")
+        with open(specimentype_json, "w") as f:
+            subprocess.run([sys.executable, "tools/vocab.py", "-s", "cache/vocabularies.db", "uijson", "spec:specimentypevocabulary", "-e"], stdout=f)
 
 
 if __name__ == "__main__":
