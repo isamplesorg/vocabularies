ae8e9b8df332844396db20b96a294a90c9cc109d
Danny Mandel <danny.mandel@gmail.com>
2024-01-24 15:07:09 -0700

    Run a sanity check checking that the UIJSON files exist after running the vocabulary action

---
+++ tests/integration_test.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+def _assert_file_exists_and_is_json(file_path: str):
+	exists = os.path.exists(file_path)
+	assert exists is True
+	with open(file_path, "r") as file:
+		json.load(file)
+
+
+def test_uijson_sample_type_exists():
+	_assert_file_exists_and_is_json("/github/workspace/src/CVJSON/docs/material_sample_type.json")
+
+def test_uijson_material_type_exists():
+	_assert_file_exists_and_is_json("/github/workspace/src/CVJSON/docs/material_type.json")
+
+def test_uijson_samples_feature_type_exists():
+	_assert_file_exists_and_is_json("/github/workspace/src/CVJSON/docs/sampled_feature_type.json")
