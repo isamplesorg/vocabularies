d7d1d18c80b3b1e921febb2f94f4f72a2dc4ba2f
Danny Mandel <danny.mandel@gmail.com>
2024-01-26 09:04:09 -0700

    Add some comments to the integration tests

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -2,6 +2,7 @@ import json
 import os
 
 def _assert_file_exists_and_is_json(file_path: str):
+	"""For the UIJSON action, assert that we wrote out the specified file, and it's valid JSON"""
 	exists = os.path.exists(file_path)
 	assert exists is True
 	with open(file_path, "r") as file:
@@ -18,4 +19,6 @@ def test_uijson_samples_feature_type_exists():
 	_assert_file_exists_and_is_json("CVJSON/docs/sampled_feature_type.json")
 
 def test_docs_directory_exists():
+	# For the docs action, just assert that the docs directory exists.
+	# TODO: Can we make this check a bit more stringent?
 	assert os.path.exists("docs")
